9315fe965cc98f232554772e65f478701ae4805e
SomeOne <56264515+someoneonearthwholovestg@users.noreply.github.com>
2020-10-15 05:10:22 +0530

    Update server.py

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ active_users = ListUsers()
 
 def start(update: tg.Update, context: tg_ext.CallbackContext):
 
-    update.message.reply_text(f"Hello {update.effective_user.username}!")
+    update.message.reply_text(f"Hello {update.effective_user.username}! \n Welcome to IMG TO PDF BOT..!! \n\n ```This Bot will help you to convert images to pdf & Enable's you to remame your pdf file..☺️``` \n\n  OWNED BY THIS AWESOME PERSON..🙈")
     active_users.append(User(tg_user=update.effective_user, language=ans.ENG))
 
     choose_language(update, context)
